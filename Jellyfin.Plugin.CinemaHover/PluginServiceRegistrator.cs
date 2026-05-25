5a62a7210b44b65050f9654a3b28ac09a602d7d7
tim <cc9jjvjvgg@privaterelay.appleid.com>
2026-05-25 08:36:24 +0200

    Fix JavaScript Injector auto-registration startup

---
+++ Jellyfin.Plugin.CinemaHover/PluginServiceRegistrator.cs
@@ -0,0 +1,16 @@
+using Jellyfin.Plugin.CinemaHover.Services;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.CinemaHover;
+
+public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(
+        IServiceCollection serviceCollection,
+        IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddHostedService<JavaScriptInjectorRegistrationService>();
+    }
+}
